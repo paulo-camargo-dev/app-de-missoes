9c444fd8833ffef324b2e9d00cc88ca06110189c
Paulo Camargo <p.camargo1508@gmail.com>
2026-02-22 16:31:27 -0300

    Enhance loadComponent with callback and add menu toggle
    Updated loadComponent to accept a callback function for post-load execution and added ativarMenu function to handle menu toggle.

--- js/include.js
+++ js/include.js
@@ -1,11 +1,22 @@
-function loadComponent(id, file) {
+function loadComponent(id, file, callback) {
   fetch(file)
     .then(res => res.text())
     .then(data => {
       document.getElementById(id).innerHTML = data;
+      if (callback) callback(); // executa depois de carregar
     });
 }
 
-loadComponent("navebar", "components/navebar.html");
+function ativarMenu() {
+  const toggle = document.querySelector(".menu-toggle");
+  const menu = document.querySelector(".menu");
 
+  if (toggle && menu) {
+    toggle.addEventListener("click", () => {
+      menu.classList.toggle("ativo");
+    });
+  }
+}
+
+loadComponent("navbar", "components/navebar.html", ativarMenu);
 loadComponent("footer", "components/footer.html");
